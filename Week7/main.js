0ce669b72d48e39b6a1d5e450f781b3e17455b60
Gael Melo <gael@gml.sh>
2026-05-05 18:22:45 -0700

    Week7: fix city/location querying

--- Week7/main.js
+++ Week7/main.js
@@ -1,32 +1,33 @@
 const locationLabel = document.getElementById("location");
+let latitude, longitude = getLocation();
 
-getLocation();
-let latitude, longitude = null;
-
-// Figure out
-if (navigator.geolocation) {
+// Figure out user location
+function getLocation() {
     navigator.geolocation.getCurrentPosition( 
     // Success
-    position => {
+    async position => {
         // Try to get city from latitude/longitude
         latitude = position.coords.latitude;
         longitude = position.coords.longitude;
         try {
-            const url = `https://bigdatacloud.net?latitude=${latitude}&longitude=${longitude}&localityLanguage=en`;
+            const url = `https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${latitude}&longitude=${longitude}&localityLanguage=en`;
             const response = await fetch(url);
-            const data = response.json();
+            const data = await response.json();
             const city = data.city;
             locationLabel.textContent = city;
         } catch (error) {
             console.log("Error finding city: " + error);
-            locationLabel.textContent = `Latitude: ${latitude}, Longitude: ${longitude}`;
+            const decimals = 2;
+            const latStr = latitude.toFixed(decimals).toString();
+            const lngStr = longitude.toFixed(decimals).toString();
+            locationLabel.textContent = `Latitude: ${latStr}, Longitude: ${lngStr}`;
         }
-        
+        return latitude, longitude;
     }, 
     // Failure
     () => {
         alert("Couldn't get your location. Try checking the website permissions!");
+        locationLabel.textContent = "Unknown Location";
+        return null;
     });
-} else {
-    locationLabel.textContent = "Unknown Location"
 }
